c861bc5ac0eda0b9ae4f33bfedc82efbe0441eae
Alexis Peralta <agpp1993@outlook.com>
2021-05-25 11:54:43 -0500

    Evitando error de dependencia mongo

--- src/main/java/com/backend/practica/PracticaApplication.java
+++ src/main/java/com/backend/practica/PracticaApplication.java
@@ -1,9 +1,13 @@
 package com.backend.practica;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
+// @EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class PracticaApplication {
 
 	public static void main(String[] args) {
